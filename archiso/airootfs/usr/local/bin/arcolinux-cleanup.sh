d636c43afaee8236ed6110a791b01c24b6f35a11
Erik Dubois <erik.dubois@gmail.com>
2020-01-18 18:37:18 +0100

    making sure everything is online

--- archiso/airootfs/usr/local/bin/arcolinux-cleanup.sh
+++ archiso/airootfs/usr/local/bin/arcolinux-cleanup.sh
@@ -10,5 +10,9 @@ rm /root/{.automated_script.sh,.zlogin}
 rm /etc/mkinitcpio-archiso.conf
 rm -r /etc/initcpio
 rm -rf /etc/lightdm
+#cleanup xfce
+for i in `ls /home/`; do rm -rf /home/$i/.config/* || exit 0; done
+for i in `ls /home/`; do rm -rf /home/$i/.local/share/xfce4 || exit 0; done
+pacman -Rs exo thunar xfconf libxfce4ui libxfce4util --noconfirm
 rm /usr/local/bin/arcolinux-all-cores.sh
 rm /usr/local/bin/arcolinux-cleanup.sh
